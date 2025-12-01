cc2f5219a1e3e1ff32cb25220f3fcdb6f8a75a42
Suprada-2002 <supradatripathy789@gmail.com>
2025-12-01 21:55:27 +0530

    adding data.js

---
+++ src/lib/data/data.js
@@ -0,0 +1,6 @@
+export const navItems = [
+  { label: "Features", href: "#feature" },
+  { label: "Workflow", href: "#workflow" },
+  { label: "Pricing", href: "#pricing" },
+  { label: "Testimonials", href: "#testimonials" },
+];
