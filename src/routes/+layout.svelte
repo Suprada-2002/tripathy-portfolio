eebf1abde690130596152d36bf9505ab6a825b41
Suprada-2002 <supradatripathy789@gmail.com>
2025-12-01 21:55:52 +0530

    adding taiwlind, font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+  import Nav from '$lib/components/nav.svelte';
+ 	import "../app.css";
 	let { children } = $props();
 </script>
 
@@ -8,4 +9,5 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Nav />
 {@render children()}
