0957adcbff806dbf7a8b40c322a51291d4ab9243
Suprada-2002 <supradatripathy789@gmail.com>
2025-12-01 22:46:52 +0530

    sketelon for devloper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,11 @@
 
 </script>
 
+<svelte:head>
+    <title>Tripathy.dev</title> 
+</svelte:head>
+
+
 <div class="max-w-7xl mx-auto pt-20 px-6">
 <Hero />
 <Services />
---
+++ src/routes/developers/ayushman/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>Ayushman Tripathy</title> 
+</svelte:head>
+
+<section class="max-w-7xl mx-auto px-6">
+    <div>
+        <p>Hi My Name is Ayushman Tripathy</p>
+    </div>
+</section>
